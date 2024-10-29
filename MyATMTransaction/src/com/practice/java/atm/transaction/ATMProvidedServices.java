d662e19c04bdaae73d0d08855a7ebb41376989b0
sairam06gv <78942972+sairam06gv@users.noreply.github.com>
2024-10-30 00:00:29 +0530

    Update ATMProvidedServices.java

--- MyATMTransaction/src/com/practice/java/atm/transaction/ATMProvidedServices.java
+++ MyATMTransaction/src/com/practice/java/atm/transaction/ATMProvidedServices.java
@@ -1,11 +1,34 @@
 package com.practice.java.atm.transaction;
 
+// Interface defining the services provided by an ATM
 public interface ATMProvidedServices {
-	
-	public void userDetails(int c_id,String pwd);
-	public void balanceEnquiry();
-	public void deposit(double amount);
-	public void withdrawal(double amount);
-	public void viewMiniStatement();
 
+    /**
+     * Validates user details based on customer ID and password.
+     * @param c_id - Customer ID of the user
+     * @param pwd - Password of the user
+     */
+    public void userDetails(int c_id, String pwd);
+
+    /**
+     * Retrieves and displays the balance for the user.
+     */
+    public void balanceEnquiry();
+
+    /**
+     * Adds a specified amount to the user's account balance.
+     * @param amount - The amount to deposit
+     */
+    public void deposit(double amount);
+
+    /**
+     * Withdraws a specified amount from the user's account balance, if sufficient funds are available.
+     * @param amount - The amount to withdraw
+     */
+    public void withdrawal(double amount);
+
+    /**
+     * Displays a mini-statement of recent transactions for the user.
+     */
+    public void viewMiniStatement();
 }
